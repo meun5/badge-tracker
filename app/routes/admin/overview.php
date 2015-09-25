f2727bad0414a070fa49487495899d9d50fafd18
meun5 <clamy4@msn.com>
2015-09-25 17:13:22 -0500

    removed dev code

--- app/routes/admin/overview.php
+++ app/routes/admin/overview.php
@@ -1,11 +1,7 @@
 <?php
 
-use app\User\UserPermission;
-
 $app->get('/admin', $admin(), function () use ($app) {
     $scouts = $app->scouts->where('active', true)->get();
-
-    //$scouts = json_decode(base64_decode($scouts->metadata));
 
     $app->render('/admin/scouts.twig', [
         'scouts' => $scouts,
